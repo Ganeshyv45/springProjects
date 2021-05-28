29c0584ca1e821ce7b80cc396ab6180ac25fb442
ganes <ganes@LAPTOP-B9JT6RIP>
2021-05-29 01:18:03 +0530

    MSG-989 latest Modification

--- src/main/java/in/naresh/ganesh/runner/MessageRunner.java
+++ src/main/java/in/naresh/ganesh/runner/MessageRunner.java
@@ -4,13 +4,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MessageRunner implements CommandLineRunner {
-
+   public static void printMessage(String msg) {
+	   System.out.println(msg);
+   }
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("WELCOME TO SPRINGBOOT APP");
 		int a=5;
 		System.out.println(a);
-		
+		printMessage("jessie");
 	}
 
 }
